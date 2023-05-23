6e4db73d3188979999abcb2e2786531ec92d2517
juanathebanana <91966932+juanathebanana@users.noreply.github.com>
2023-05-23 12:54:56 +0200

    Update website.py

--- website.py
+++ website.py
@@ -17,7 +17,6 @@ from storing.dirit import check_hashes
 
 nuevo=open('/home/juana/Desktop/coursera/storing/subject_list.pkl','rb')
 cursos_nuevo=pickle.load(open('subject_list.pkl','rb')
-
 completo=list(set(cursos_nuevo['Course Subject'].values()))
 
 
